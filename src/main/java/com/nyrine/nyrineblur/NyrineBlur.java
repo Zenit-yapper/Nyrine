1a6a9ff129f0cfc3bff90ef4351ed26555a9d8e9
Zenit-yapper <zenitog69@gmail.com>
2026-05-21 19:46:38 +0530

    Create NyrineBlur.java

---
+++ src/main/java/com/nyrine/nyrineblur/NyrineBlur.java
@@ -0,0 +1,12 @@
+package com.nyrine.nyrineblur;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NyrineBlur implements ModInitializer {
+    public static final String MOD_ID = "nyrineblur";
+
+    @Override
+    public void onInitialize() {
+        // Server-side if needed (currently client-only)
+    }
+}
